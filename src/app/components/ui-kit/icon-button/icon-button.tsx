b08cc105c2d628e5b4de11fa80931571e3d90c1a
rittiev <ramazzan.ittiev@gmail.com>
2025-02-07 12:06:49 +0000

    Added icon button markup and styles

---
+++ src/app/components/ui-kit/icon-button/icon-button.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import cn from "classnames";
+
+import "./icon-button.css";
+
+interface IconButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  icon: React.ReactNode;
+}
+
+const baseClass = "icon-button";
+
+export const IconButton = (props: IconButtonProps) => {
+  const { icon, ...buttonProps } = props;
+
+  const classNames = cn(baseClass, props.className);
+
+  return (
+    <button
+      {...buttonProps}
+      aria-hidden={true}
+      className={classNames}
+      type="button"
+    >
+      {icon}
+    </button>
+  );
+};
